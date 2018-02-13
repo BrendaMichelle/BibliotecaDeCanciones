a4e0f9dda78a2ba79207c54aaf7b0f2377289297
Brenda <BrendaMichelle@users.noreply.github.com>
2018-02-13 18:28:24 -0500

    Add files via upload

--- ListViewController.java
+++ ListViewController.java
@@ -1,37 +1,25 @@
 package application;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.*;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
-import java.util.Optional;
 import application.Songs.Song;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TextInputDialog;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-
-/**
- * 
- * Sets up either a blank List View
- * or populates from text file that is
- * found within the directory that
- * the Song Library is located in
- * 
- * Also is the middle man between the users
- * inputs and the corresponding actions that
- * should be taken
- * 
- */
 public class ListViewController {
 	// All the buttons and textfields that that app uses
 	@FXML Button editButton;
@@ -45,100 +33,65 @@ public class ListViewController {
 	@FXML TextField artistDetails;
 	@FXML TextField albumDetails;
 	@FXML TextField yearDateDetails;
+	@FXML ListView<String> listView; // The ListView object that populates the song library
+	private ObservableList<String> obsList; //list that's put into ListView
+	private ArrayList<Song> songArray; //Contains Song objects with their deets
 	
-	// The ListView object that populates the song library
-	@FXML ListView<String> listView;
-	
-	// Both ArrayList. The first being the list that gets
-	// put into the ListView
-	// While songArray contains the objects of Song
-	// with all their details
-	private ObservableList<String> obsList;
-	private ArrayList<Song> songArray;
-	
-	/**
-	 * 
-	 * @param mainStage
-	 * The main scene or window that the user
-	 * will mainly be interacting with
-	 * @param file
-	 * The file that the program will search for to
-	 * read from and populate the ListView
-	 * 
-	 * The setup of the ListView
-	 * i.e. either opens a blank ListView
-	 * if no text file is given or 
-	 * populates the ListView if a valid file is
-	 * found
-	 */
-	public void start(Stage mainStage, File file) {
-		songArray = new ArrayList<Song>();
+	public void start(Stage mainStage, File file, ArrayList<Song> songArray) throws FileNotFoundException {
+		//songArray = new ArrayList<Song>();
 		obsList = FXCollections.observableArrayList();
 		newSong.setText(null);
 		newArtist.setText(null);
 		newAlbum.setText(null);
 		newYearDate.setText(null);
+		this.songArray = songArray;
 		
-		
-		//*** Still need code to search in the directory that the current code is running 
-		//*** for a specified file that we have created in a previous run
-		//*** If file is found (either in this class or passed to us in the parameter)
-		//*** must then read through file and create the corresponding Song objects
-		//*** with correct field values
-		//*** NOTE: Song and Artist MUST be present in file to be considered valid
-		//***		If we do not encounter an album, we pass null for album parameter
-		//***		We MUST encounter a year date for each Song (either as positive int or as negative which typically should be -1)
-		
-		
-		//*** Maybe we can set up our files to read as: Song: "Song Name", Artist: "Artist Name", Album: "" (or) "Album Name", Year: "-1" (or) "0-oo"
-		//*** But whatever is easiest
-		
-		
-		
-		//***The following is just to test out the app
-		//*** And should be deleted once completed with assignment
-		//*** In the meantime, use this as a template as to how to
-		//*** treat the relation ship between the songArray and
-		//*** the observable List
-		
-		//*** Template begins here
-		
-		// Creating objects to test inputting into both songArray and ListView
-		Song despacito = new Song("Despacito", "Daddy Fonsi", null, -1);
-		Song newViolence = new Song("The New Violence", "Emanuel", "Soundtrack to a HeadRush", 2005);
-		Song tobeLoved = new Song("To be Loved", "Papa Roach", "The Paramour Sessions", 2006);
-		Song ratedR = new Song("Rated R", "The White Noise", "AM/PM", 2017);
-		songArray.add(despacito);
-		songArray.add(newViolence);
-		songArray.add(tobeLoved);
-		songArray.add(ratedR);
-		
+
 		// Creating the String form from the given songArray to put into the observable LIst 
+		
 		for(int i = 0 ; i < songArray.size() ; i++) {
 			String enterList = songArray.get(i).name + "    :    " + songArray.get(i).artist;
 			obsList.add(enterList);
 		}
-		
 		//Put Strings of songs (if any) into ListView and select the first element
 		listView.setItems(obsList);
 		listView.getSelectionModel().select(0);
 		
+	
 		// Display the selected song (should be the first element if already populated)
 		// However, must take into account of no text File to read from so Selected Song Detail Fields should all be null
+		if(songArray.size() > 0) {
 		songDetails.setText(songArray.get(0).name);
 		artistDetails.setText(songArray.get(0).artist);
 		albumDetails.setText(songArray.get(0).album);
+		}
 		// Song objects will always have an int for years where >0 indicates a valid year and <0 (usually -1) indicates no year given
 		// So we give the text field of detailed year date null to make up for it
-		if(songArray.get(listView.getSelectionModel().getSelectedIndex()).year > 0) {
+		if(songArray.size() > 0 && songArray.get(listView.getSelectionModel().getSelectedIndex()).year > 0) {
 			yearDateDetails.setText("" + songArray.get(0).year);
+
 		}
 		else {
 			yearDateDetails.setText(null);
-
 		}
 		
-		//*** Template ends here
+		mainStage.setOnHidden(event -> {try {
+			saveFile(file, songArray);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}} );
+		
+		
+	}
+	
+	public void saveFile(File file,  ArrayList<Song> updatedList ) throws FileNotFoundException {
+		PrintWriter pw = new PrintWriter(file);
+		if(updatedList.size() > 0){
+		for( int i = 0; i < updatedList.size(); i ++){
+		pw.println(updatedList.get(i).name + "~" + updatedList.get(i).artist + "~" + updatedList.get(i).album + "~" + updatedList.get(i).year);
+		}
+		}
+		pw.close();
 	}
 	
 	/**
@@ -157,7 +110,6 @@ public class ListViewController {
 	 */
 	public void handleArrowButtons(KeyEvent event) {
 		if(event.getCode() == KeyCode.UP || event.getCode() == KeyCode.DOWN) {
-			//System.out.println("Works, index is: " + listView.getSelectionModel().getSelectedIndex());
 			songDetails.setText(songArray.get(listView.getSelectionModel().getSelectedIndex()).name);
 			artistDetails.setText(songArray.get(listView.getSelectionModel().getSelectedIndex()).artist);
 			albumDetails.setText(songArray.get(listView.getSelectionModel().getSelectedIndex()).album);
@@ -173,12 +125,13 @@ public class ListViewController {
 		else if(event.getCode() == KeyCode.LEFT || event.getCode() == KeyCode.RIGHT){
 			//event.
 		}
+		
 	}
 	
 	/**
 	 * 
 	 * @param e
-	 * The actual even taken place
+	 * The actual event taken place
 	 * on a Mouse Click
 	 * 
 	 * Handles the user clicking on a
@@ -193,6 +146,8 @@ public class ListViewController {
 	 */
 	public void handleMouseClick(MouseEvent e) {
 		// Populates the Selected Song Details portion to the selected element
+		
+		
 		songDetails.setText(songArray.get(listView.getSelectionModel().getSelectedIndex()).name);
 		artistDetails.setText(songArray.get(listView.getSelectionModel().getSelectedIndex()).artist);
 		albumDetails.setText(songArray.get(listView.getSelectionModel().getSelectedIndex()).album);
@@ -233,16 +188,10 @@ public class ListViewController {
 	}
 	
 	/**
-	 * 
-	 * @param primaryStage
-	 * Used to be able to give the specific
-	 * error message pop up or pop up in general
-	 * 
 	 * Corresponds to the DeleteButton and takes
 	 * the index of the ListView to be able to
 	 * remove it from both the observable list
-	 * AND songArray
-	 * 
+	 * AND songArray 
 	 * Also sets the Selected Song Details text boxes to empty as
 	 * a default behavior
 	 */
@@ -260,7 +209,7 @@ public class ListViewController {
 			alert.setTitle("Delete Song");
 			alert.setHeaderText("ARE YOU SURE YOU WANT TO DELETE:");
 			String content = "Song: " + songArray.get(listView.getSelectionModel().getSelectedIndex()).name + 
-					"\nBy: " + songArray.get(listView.getSelectionModel().getSelectedIndex()).artist +
+					"\nArtist: " + songArray.get(listView.getSelectionModel().getSelectedIndex()).artist +
 					"\nIndex: " + listView.getSelectionModel().getSelectedIndex();
 			alert.setContentText(content);
 			Optional<ButtonType> result = alert.showAndWait();
@@ -301,11 +250,6 @@ public class ListViewController {
 	}
 	
 	/**
-	 * 
-	 * @param primaryStage
-	 * Uses the stage to give any
-	 * needed pop up messages
-	 * 
 	 * The functionality of adding a new 
 	 * song to both songArray and the 
 	 * observable List in order to update
@@ -323,7 +267,7 @@ public class ListViewController {
 			Alert alert = new Alert(AlertType.ERROR);
 			alert.initOwner(primaryStage);
 			alert.setTitle("Error");
-			alert.setHeaderText("Please Enter Both a Song Name and Artist!");
+			alert.setHeaderText("Please Enter Both a Song Name and an Artist!");
 			String content = "Song name and Artist must be filled and cannot begin with a space.";
 			alert.setContentText(content);
 			alert.showAndWait();
@@ -356,7 +300,7 @@ public class ListViewController {
 			Alert alert = new Alert(AlertType.ERROR);
 			alert.initOwner(primaryStage);
 			alert.setTitle("Incorrect Input");
-			alert.setHeaderText("Please Enter a Valid Year Date!");
+			alert.setHeaderText("Please Enter a Valid Year Date! Format: ####");
 			String content = "Empty spaces and letters are not allowed.";
 			alert.setContentText(content);
 			alert.showAndWait();
@@ -368,21 +312,22 @@ public class ListViewController {
 		newGivenArtist = newArtist.getText();
 		newGivenAlbum = newAlbum.getText();
 		
+		//else add and display song into list alphabetically
 		Song songToAdd = new Song(newGivenSong, newGivenArtist, newGivenAlbum, newGivenYearDate);
-		
-		//*** Here is where we must go through the Song Array List
-		//*** And alphabetically insert the new Song object
-		//*** We must also save the index that the song went into
-		//*** the songArray so that we may also add it into the
-		//*** same index of the observableList but in String form:
-		//*** "Song	:	Artist"
+		songArray.add(songToAdd);
+		Collections.sort(songArray, Song.Comparators.NAMEANDARTIST);
+		obsList.clear();
+		for(int i = 0 ; i < songArray.size() ; i++) {
+			String enterList = songArray.get(i).name + "    :    " + songArray.get(i).artist;
+			obsList.add(enterList);
+		}
+		listView.setItems(obsList);
+
 	}
 	
+
+	
 	/**
-	 * 
-	 * @param primaryStage
-	 * Used to give any needed pop up messages
-	 * 
 	 * Takes all the information from the text fields
 	 * of the Selected Song Details and puts them into
 	 * both the songArray and observable List in
@@ -443,7 +388,7 @@ public class ListViewController {
 			Alert alert = new Alert(AlertType.ERROR);
 			alert.initOwner(primaryStage);
 			alert.setTitle("Incorrect Input");
-			alert.setHeaderText("Please Enter a Valid Year Date!");
+			alert.setHeaderText("Please Enter a Valid Year Date! Format: ####");
 			String content = "Empty spaces and letters are not allowed.";
 			alert.setContentText(content);
 			alert.showAndWait();
@@ -453,25 +398,50 @@ public class ListViewController {
 			else {
 				yearDateDetails.setText(null);
 			}
+			
+			
 			return;
 		}
 		
-		//*** The bottom logic simply replaces the song object in the same index as the selected index
-		//*** in the song Array List and obsList. This is not fully correct.
-		//*** Must take into account if user totally changes either the song name
-		//*** artist name or BOTH. Must then remove the current Song at the index and 
-		//*** then traverse the songArray to put the newly edited song into alphabetical order
-		//*** Once alphabetical index is found, must then put the song into the same index as
-		//*** the obsList array but as String of: "Song	:	artist" 
+
 		editedName = songDetails.getText();
 		editedArtist = artistDetails.getText();
 		editedAlbum = albumDetails.getText();
 		
 		Song editedSong = new Song(editedName, editedArtist, editedAlbum, editedYearDate);
 		int index = listView.getSelectionModel().getSelectedIndex();
-		songArray.set(index, editedSong);
-		obsList.set(index,  songArray.get(index).name + "    :    " + songArray.get(index).artist);
 		
+		//delete from list and add edited version
+		 if(index + 1 == obsList.size()) {
+			 obsList.remove(index);
+			 songArray.remove(index);
+			 index--;
+			 listView.getSelectionModel().select(index);
+			 songDetails.setText("");
+			 artistDetails.setText("");
+			 albumDetails.setText("");
+			 yearDateDetails.setText("");
+		 }
+		 else {
+			 obsList.remove(index);
+			 songArray.remove(index);
+			 listView.getSelectionModel().select(index);
+			 songDetails.setText("");
+			 artistDetails.setText("");
+			 albumDetails.setText("");
+			 yearDateDetails.setText("");
+		 }
+		songArray.add(editedSong);
+		//alphabetize
+		Collections.sort(songArray, Song.Comparators.NAMEANDARTIST);
+		obsList.clear();
+		for(int i = 0 ; i < songArray.size() ; i++) {
+			String enterList = songArray.get(i).name + "    :    " + songArray.get(i).artist;
+			obsList.add(enterList);
+		}
+		listView.setItems(obsList);
+		
+
 		Alert alert = new Alert(AlertType.INFORMATION);
 		alert.initOwner(primaryStage);
 		alert.setTitle("Song Edited");
@@ -482,4 +452,6 @@ public class ListViewController {
 		alert.showAndWait();
 			
 	}
+	
+
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 package application;
 	
 import java.io.File;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+import application.Songs.Song;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -16,17 +20,28 @@ public class Main extends Application {
 			FXMLLoader loader = new FXMLLoader();   
 			loader.setLocation(getClass().getResource("/application/ListView.fxml"));
 			AnchorPane root = (AnchorPane)loader.load();
-
-
 			ListViewController listController = loader.getController();
-			// Either find file here and pass through parameter
-			// Or let listController find the file
-			//But I think it would be best if the Main found the file
-			File file = null;
-			listController.start(primaryStage, file);
-
+			ArrayList<Song> songlist = new ArrayList<Song>();
+			
+			/*Fills ArrayList called songlist if songlist.txt exists && is not empty
+			 * songlist is in alphabetical order upon filling.*/
+			File file = new File("songlibrary.txt"); //C:/Users/Belle/Desktop/
+			if(file.exists() && file.isFile()){
+				Scanner sc = new Scanner(file);
+				while(sc.hasNextLine()){
+					String ln = sc.nextLine();
+					String arr [] = ln.split("~");
+					songlist.add(new Song(arr[0], arr[1], arr[2], Integer.parseInt(arr[3])));
+				}
+				sc.close();
+			}
+			else{ 
+				System.out.println("File doesn't exist. New file being created...");
+				file.createNewFile();
+				//file.getParentFile().mkdirs();
+			}
+			listController.start(primaryStage, file, songlist);
 			Scene scene = new Scene(root,700,550);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("Song Library");
 			primaryStage.setScene(scene);
 			primaryStage.show();
